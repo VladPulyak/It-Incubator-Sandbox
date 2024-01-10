2183ce373a5ac5bd68dd4dc652ff0e2e9da23745
VladPulyak <vlad.pulyak03@mail.ru>
2024-01-10 16:55:53 +0300

    Added COdeWars task

---
+++ CodeWars/kyu8/Double Char.js
@@ -0,0 +1,19 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+function doubleChar(str) {
+    let arr = str.split('');
+    let result = [];
+    for(let i = 0; i < arr.length - 1; i++){
+        result.push(arr[i]);
+        result.push(arr[i]);
+    }
+    return result.join("");
+}
+console.log(doubleChar('dsdfsdffsd'))
+  
